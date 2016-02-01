f1cdfa626c5f0b70ff29a3ffcba1af7449fe7d3a
Alexandr Demin <Saydos@mail.ru>
2016-02-02 00:05:25 +0300

    Ignoring and deleting config files.

---
+++ CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/source/src/main.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/RegexAST/RegexASTNode.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/RegexAST/RegexASTParser.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/RegexAST/RegexASTAnnotationEvaluator.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/ArrayTypeInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/BasicTypeInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/Context.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/CustomTypeInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/EnumerationTypeInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/LabelInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/Module.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/VarInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/GlobalContext.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/StructTypeInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/IR/IntegerTypeInfo.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/Grammar/Terminals.cpp.o"
+  "CMakeFiles/compiler.dir/source/src/PascalCompiler/Grammar/Nonterminals.cpp.o"
+  "bin/compiler.pdb"
+  "bin/compiler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
